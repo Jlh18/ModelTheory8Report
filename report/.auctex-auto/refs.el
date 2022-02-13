f2ec74ffe83d8ce0c5304b9a4124c56c48a94e90
jlh18 <josephhua73@yahoo.com>
2022-02-13 12:26:03 +0000

    until realization of terms and formulas.

---
+++ report/.auctex-auto/refs.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "refs"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "marker"
+    "poizat"
+    "tent"
+    "buechler"
+    "openlogicproject"
+    "pillay"
+    "atiyah"
+    "hungerford"
+    "stack0"
+    "wiki0"
+    "jech"
+    "fandom0"
+    "henson"
+    "hodges"
+    "viswanathan"
+    "wiki1"
+    "flypitch"))
+ :bibtex)
+
